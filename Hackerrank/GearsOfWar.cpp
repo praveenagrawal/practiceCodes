aeb06f8cd7c9fa718cf37f9de0010bfb1d62b45a
praveenagrawal <praveen.agrawal0@gmail.com>
2016-09-13 00:07:47 +0530

    update

---
+++ Hackerrank/GearsOfWar.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int q;
+    cin>>q;
+    while(q--)
+	{
+		int n;
+		cin>>n;
+		if(n%2==0)
+			cout<<"Yes\n";
+		else
+			cout<<"No\n";
+	} 
+    return 0;
+}
--- Hackerrank/LarrysArray.cpp
+++ Hackerrank/LarrysArray.cpp
@@ -8,6 +8,28 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    
+    int t;
+    cin>>t;
+    while(t--)
+	{
+		int n;
+		cin>>n;
+		std::vector<int> A(n);
+		for(int i=0;i<n;i++)
+			cin>>A[i];
+		int count = 0;
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(A[i]>A[j])
+					count++;
+			}
+		}
+		if(count%2==0)
+			cout<<"YES\n";
+		else
+			cout<<"NO\n";
+	}
     return 0;
 }
