8e3ef892911e34a65ec8c90cc124be8c52863819
praveenagrawal <praveen.agrawal0@gmail.com>
2016-09-14 20:31:05 +0530

    another version

--- Hackerrank/ShortPalindrome.cpp
+++ Hackerrank/ShortPalindrome.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct listCell
 {
 	int count=0;
-	int location=-1;
+	std::vector<int> location;
 };
 
 
@@ -18,33 +18,28 @@ int main() {
     cin>>s;
     int n = s.length();
     unsigned long long count = 0;
-    std::vector< vector<int> > preList(n, vector<int>(26, 0));
-    std::vector< vector<int> > postList(n, vector<int>(26, 0));
+    std::vector< vector<listCell> > preList(n, vector<listCell>(26));
+    std::vector< vector<listCell> > postList(n, vector<listCell>(26));
    	for(int i=1;i<n;i++)
 	{
 		for(int j=0;j<26;j++)
 			preList[i][j]=preList[i-1][j];
-		preList[i][s[i-1]-'a']++;
-	//	for(int j=0;j<=i-1;j++)
-	//		preList[i][s[j]-'a']++;
+		preList[i][s[i-1]-'a'].count++;
+		preList[i][s[i-1]-'a'].location.push_back(i-1);
 	}
 	for(int i=n-2;i>=0;i--)
 	{
 		for(int j=0;j<26;j++)
 			postList[i][j]=postList[i+1][j];
-		postList[i][s[i+1]-'a']++;
-	//	for(int j=n-1;j>i;j--)
-	//		postList[i][s[j]-'a']++;
+		postList[i][s[i+1]-'a'].count++;
+		postList[i][s[i+1]-'a'].location.push_back(i+1);
 	}
 	for(int i=1;i<n-2;i++)
 	{
-		for(int j=i+1;j<n-1;j++)
+		for(int j=0;j<postList[i][s[i]-'a'].location.size();j++)
 		{
-			if(s[j]==s[i])
-			{
-				for(int k=0;k<26;k++)
-					count = (count + preList[i][k]*postList[j][k])%(1000000007);
-			}
+			for(int k=0;k<26;k++)
+				count = (count + preList[i][k].count*postList[postList[i][s[i]-'a'].location[j]][k].count)%(1000000007);
 		}
 	}
 	cout<<count;
