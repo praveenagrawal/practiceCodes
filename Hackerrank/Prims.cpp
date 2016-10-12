a2f9ccec6f72c346b9f73faf0229709551b2e2f6
praveenagrawal <praveen.agrawal0@gmail.com>
2016-10-13 01:14:18 +0530

    done

--- Hackerrank/Prims.cpp
+++ Hackerrank/Prims.cpp
@@ -7,26 +7,21 @@
 #include <limits.h>
 using namespace std;
 
-struct edge
-{
-	int u;
-	int v;
-	int w;
-};
 
 struct node
 {
+	int v;
 	std::vector<int> adj;
 	std::vector<int> w;
 	int key = INT_MAX;
-	int popped = false;
+	int parent=-1;
 };
 
 struct compare
 {
-  bool operator()(const node *l, const node *r)
+  bool operator()(const node l, const node r)
   {
-      return (l->key > r->key);
+      return (l.key > r.key);
   }
 };
 
@@ -35,7 +30,8 @@ int main() {
     int n, m;
     cin>>n>>m;
     std::vector<node> graph(n);
-    priority_queue<node*,vector<node*>, compare > Q;
+    priority_queue<node,vector<node>, compare > Q;
+    std::vector<bool> inMST(n, 0);
     for(int i=0;i<m;i++)
 	{
 		int x,y,r;
@@ -45,27 +41,35 @@ int main() {
 		graph[y].adj.push_back(x);
 		graph[x].w.push_back(r);
 		graph[y].w.push_back(r);
+		graph[x].v = x;
+		graph[y].v = y;
 	}
 	int s;
 	cin>>s;
 	s--;
 	graph[s].key = 0;
-	for(int i=0;i<n;i++)
-		Q.push(&graph[i]);
+	Q.push(graph[s]);
 	unsigned long long total = 0;
 	while(Q.size()!=0)
 	{
-		node *u = Q.top();
+		node u = Q.top();
 		Q.pop();
-		u->popped = true;
-		if(u->key!=INT_MAX)
-			total+=u->key;
-		for(int i=0;i<u->adj.size();i++)
+		inMST[u.v] = 1;
+		for(int i=0;i<u.adj.size();i++)
 		{
-			int v = u->adj[i];	
-			if(graph[v].popped==false && (u->w[i] < graph[v].key))
-				graph[v].key = 	u->w[i];																																																																														
+			int v = u.adj[i];
+			if(inMST[v]==0 && u.w[i]<graph[v].key)
+			{
+				graph[v].key = u.w[i];
+				graph[v].parent = u.v;
+				Q.push(graph[v]);
+			}
 		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(graph[i].parent!=-1)
+			total+=graph[i].key;
 	}
 	cout<<total;
 
